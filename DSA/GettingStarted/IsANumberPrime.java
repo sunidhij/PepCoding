610710b5675d4afa0b35b2f68f012bd84ee6bb2d
Sunidhi Jain <86722239+sunidhij@users.noreply.github.com>
2022-01-10 00:59:39 +0530

    Updated the logic

--- DSA/GettingStarted/IsANumberPrime.java
+++ DSA/GettingStarted/IsANumberPrime.java
@@ -4,6 +4,11 @@ import java.util.*;
   
   public static void main(String[] args) {
       Scanner scn = new Scanner(System.in);
+    
+      /* Logic Explanation - a number n is not a prime, it can be factored into two factors a and b:
+          n = a * b
+         Now a and b can't be both greater than the square root of n, since then the product a * b would be greater than sqrt(n) * sqrt(n) = n. So in any factorization of n, at least one of the factors must be smaller than the square root of n, and if we can't find any factors less than or equal to the square root, n must be a prime.
+*/
   
        // write ur code here
        int t =scn.nextInt();
