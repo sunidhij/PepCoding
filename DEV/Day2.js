1199549e988baaf0d940ca655a2432cf039065f8
Sunidhi Jain <86722239+sunidhij@users.noreply.github.com>
2022-01-18 22:07:13 +0530

    Create Day2.js

---
+++ DEV/Day2.js
@@ -0,0 +1,11 @@
+// For autocomplete, use tab
+
+// node is like an android phone
+// npm is the appstore
+//libraries are the apps. Some are preinstalled and the other can be installed with npm
+
+// Open/ close terminal - ctrl + `
+
+//open/close side bar ctrl + b
+
+// alt to split horizontally
